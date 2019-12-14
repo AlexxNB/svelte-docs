d552f1e2affd03f5b542f85b9da8f112ff5cc700
AlexxNB <iam@alexxnb.ru>
2019-12-14 08:17:23 +0300

    add opengraph tags

---
+++ docs_src/src/includes/opengraph.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {current_page} from '@svelte-docs/get/routes';
+    import maintitle from '@svelte-docs/get/maintitle';
+    $: title = $current_page.title ? $current_page.title + ' — ' + maintitle : maintitle;
+</script>
+
+
+<svelte:head>
+    <meta property="og:title" content={title}>
+    <meta property="og:description" content="A rapid way to document your Svelte things">
+    <meta property="og:image" content="https://alexxnb.github.io/svelte-docs/static/social.png">
+    <meta property="og:url" content="https://alexxnb.github.io/svelte-docs/{$current_page.url}">
+    <meta name="twitter:card" content="summary_large_image">
+</svelte:head>
--- docs_src/src/includes/topbar.md
+++ docs_src/src/includes/topbar.md
@@ -1,4 +1,7 @@
 import Github from './github.svelte';
+import Opengraph from './opengraph.svelte';
+
+<Opengraph />
 
 * [Docs](getting-started)
 * <Github animation url="https://github.com/alexxnb/svelte-docs"/>
