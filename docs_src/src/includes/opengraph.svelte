bf73d2dd895f17d8503c3f25c64e54d83e7f6d44
AlexxNB <iam@alexxnb.ru>
2019-12-14 08:27:20 +0300

    add twitter card

--- docs_src/src/includes/opengraph.svelte
+++ docs_src/src/includes/opengraph.svelte
@@ -1,14 +1,22 @@
 <script>
     import {current_page} from '@svelte-docs/get/routes';
     import maintitle from '@svelte-docs/get/maintitle';
+    
+    let descr = 'A rapid way to document your Svelte things';
+    let img = 'https://alexxnb.github.io/svelte-docs/static/social.png';
+
     $: title = $current_page.title ? $current_page.title + ' — ' + maintitle : maintitle;
+    $: url = `https://alexxnb.github.io/svelte-docs/${$current_page.url}`;
 </script>
 
 
 <svelte:head>
     <meta property="og:title" content={title}>
-    <meta property="og:description" content="A rapid way to document your Svelte things">
-    <meta property="og:image" content="https://alexxnb.github.io/svelte-docs/static/social.png">
-    <meta property="og:url" content="https://alexxnb.github.io/svelte-docs/{$current_page.url}">
+    <meta property="og:description" content={descr}>
+    <meta property="og:image" content={img}>
+    <meta property="og:url" content={url}>
+    <meta name="twitter:title" content={title}>
+    <meta name="twitter:description" content={descr}>
+    <meta name="twitter:image" content={img}>
     <meta name="twitter:card" content="summary_large_image">
 </svelte:head>
