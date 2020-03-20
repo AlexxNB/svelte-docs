ccc39e95b5977b350e82a148aa23271731073d2a
AlexxNB <iam@alexxnb.ru>
2020-03-20 10:04:16 +0300

    update versions

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,8 +1,8 @@
 import fs from 'fs-extra';
 import path from 'path';
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import {markdown} from 'svelte-preprocess-markdown';
