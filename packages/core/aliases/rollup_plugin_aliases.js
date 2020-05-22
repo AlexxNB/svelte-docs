aec1225e6197ce0acc9c5c98253d96af57a86cdb
AlexxNB <iam@alexxnb.ru>
2020-05-22 23:05:36 +0300

    fix https://github.com/AlexxNB/svelte-docs/issues/18

--- packages/core/aliases/rollup_plugin_aliases.js
+++ packages/core/aliases/rollup_plugin_aliases.js
@@ -8,15 +8,18 @@ export default function() {
     return {
         name: 'rollup_plugin_aliases',
 
-        resolveId(id) { return config.aliases[id] !== undefined ? id : null },
+        resolveId(id,importer) { 
+            if(id.endsWith('.svelte') && config.aliases[id] !== undefined){
+                return this.resolve(config.aliases[id],importer);
+            }
+            return config.aliases[id] ? id : null;
+        },
         load(id) { 
             if(config.aliases[id] !== undefined){
                 const pkgpath = path.resolve(config.aliases[id]);
                 if (!fs.existsSync(pkgpath)) ERR('Config.aliases: No such file',pkgpath);
 
-                if(id.endsWith('.svelte')) 
-                    return fs.readFileSync(pkgpath,{encoding:'utf-8'});
-                else if(pkgpath.endsWith('.svelte')) 
+                if(pkgpath.endsWith('.svelte')) 
                     return `export {default} from '${pkgpath}';`; 
                 else
                     return `export * from '${pkgpath}';`; 
